468667216e25d2f291a1d9c4fe1520302e8c8b29
Jaedsonn <jaedsonnm@gmail.com>
2026-01-02 21:02:55 -0300

    feat: gas station final solution

--- gasstation.cpp
+++ gasstation.cpp
@@ -7,6 +7,37 @@
 
 using namespace std;
 
+int solve2(vi& gas, vi& cost){
+    int total_gas = 0;
+    int total_cost = 0;
+    int n = gas.size();
+
+    if(n == 0) return -1;
+    if(n == 1){
+        if(gas[0] > cost[0]) return 0;
+        else return -1;
+    }
+
+    for(int i = 0; i < n; i++){
+        total_cost += cost[i];
+        total_gas += gas[i];
+    }
+
+    if(total_cost > total_gas) return -1;
+
+    int tank = 0;
+    int best = -1;
+    for(int i = 0; i < n; i++){
+        tank += gas[i] - cost[i];
+        if(tank < 0){
+            best = i + 1;
+            tank = 0;
+        }
+    }
+
+    return best;
+}
+
 // TLE
 int solve(vi& gas, vi& cost){
     int flag = -1;
@@ -44,10 +75,10 @@ int solve(vi& gas, vi& cost){
 }
 
 int main(){
-    vi a = {5,8,2,8};
-    vi b = {6,5,6,6};
+    vi a = {5,1,2,3,4};
+    vi b = {4,4,1,5,1};
 
-    int res = canCompleteCircuit(a, b);
+    int res = solve2(a, b);
 
     return 0;
 }
